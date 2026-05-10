3d603c987e1a106f06d9f471200dceb1bb75e4fc
Benjamin Ryder wismath <benjaminryderwismath@MacBook-Air-de-Benjamin.local>
2026-05-10 19:17:48 -0300

    feat: add CORS

--- src/app.js
+++ src/app.js
@@ -1,13 +1,20 @@
 
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const pagosRoutes = require("./routes/pagos.routes");
 const reportesRoutes = require("./routes/reportes.routes");
 
+app.use(cors({
+  origin: "*",  // en producción podés restringir al dominio real
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+}));
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
-res.send("Gym Management API running 🚀");
+    res.send("Gym Management API running 🚀");
 });
 
 app.use("/auth", require("./routes/auth.routes"));
@@ -20,8 +27,8 @@ app.use("/reportes", reportesRoutes);
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
     res.status(statusCode).json({
-        status: "error",
-        message: err.message || "Error interno en el servidor"
+    status: "error",
+    message: err.message || "Error interno en el servidor"
     });
 });
 
